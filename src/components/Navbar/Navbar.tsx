b0b244e28f3e9ddd091ffeb48aa5d5ebea3578b4
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-04-11 00:20:06 +0530

    Admin

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,6 +6,28 @@ import logo from './logo.png'
 import './Navbar.css'
 function Navbar() {
     const [isAdminAuthenticated,setIsAdminAuthenticated]=useState(false)
+    const checkAdminAuthenticated = async () => {
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/admin/checklogin', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include'
+        });
+        if(response.ok){
+          setIsAdminAuthenticated(true);
+        }else{
+          setIsAdminAuthenticated(false);
+        }
+      } catch (err) {
+        console.error(err);
+        setIsAdminAuthenticated(false);
+      }
+    }
+    useEffect(()=>{
+      checkAdminAuthenticated()
+    },[]);
     return (
         <div className="navbar">
           <Image src={logo} alt="Logo" width={100} className="logo" />
